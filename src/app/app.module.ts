1ddce17486405a4f0c7bd421614f9fb93bd97dcf
eyvind1 <eyvind.osco@ucsp.edu.pe>
2020-12-17 14:19:47 -0500

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Rutas
+import { APP_ROUTING } from './app.routes';
+
+//Componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { ProductosComponent } from './components/productos/productos.component';
-import { SliderComponent } from './components/slider/slider.component';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxPaginationModule } from 'ngx-pagination';
-import { EmpresasComponent } from './components/empresas/empresas.component';
 import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ProductosComponent,
-    SliderComponent,
-    EmpresasComponent,
-    AboutComponent
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     NgxPaginationModule,
     NgbModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
+    APP_ROUTING,
     
   ],
   providers: [],
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,33 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 import { FirebaseServiceService } from '../../services/firebase-service.service';
 
 
 @Component({
-  selector: 'app-productos',
-  templateUrl: './productos.component.html',
-  styleUrls: ['./productos.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class ProductosComponent implements OnInit {
-  
+export class HomeComponent implements OnInit {
 
   constructor(
     private firebaseServiceService: FirebaseServiceService,
   ) { }
 
-  
+
   collection = {count:0, data:[]}
 
   ngOnInit(): void {
 
-  
-    
-
     this.firebaseServiceService.getEmpresas().subscribe(resp=>{
       this.collection.data = resp.map((e:any)=>{
         return {
-          activo: e.payload.doc.data().activo,
           ruc: e.payload.doc.data().emp_cruc,
+          departamento: e.payload.doc.data().emp_cdepa,
           telefono: e.payload.doc.data().emp_ctelef,
         }
       })
@@ -36,9 +33,14 @@ export class ProductosComponent implements OnInit {
       console.error(error)
     }
     );
+
+
+
+
+
+
   }
 
-  
-
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
 }
